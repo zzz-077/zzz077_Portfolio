088a15a5b9e93da6241710792e5eca1daee3bcf7
zzz077 <122636696+zzz-077@users.noreply.github.com>
2024-06-06 00:59:42 +0400

    modified portfolio

--- project/src/app/lab/lab.component.ts
+++ project/src/app/lab/lab.component.ts
@@ -10,43 +10,47 @@ export class LabComponent {
   imgData = [
     {
       id: 1,
-      img: '/assets/siteN1.png',
-    },
-    {
-      id: 2,
-      img: '/assets/siteN2.png',
-    },
-    {
-      id: 3,
-      img: '/assets/siteN3.png',
-    },
-    {
-      id: 4,
-      img: '/assets/siteN4.png',
-    },
-    {
-      id: 5,
-      img: '/assets/siteN5.png',
-    },
-    {
-      id: 6,
-      img: '/assets/siteN6.png',
-    },
-    {
-      id: 7,
-      img: '/assets/siteN7.png',
-    },
-    {
-      id: 8,
-      img: '/assets/siteN8.png',
-    },
-    {
-      id: 9,
       img: '/assets/gameN1.png',
     },
     {
-      id: 10,
+      id: 2,
       img: '/assets/gameN2.png',
+    },
+    {
+      id: 3,
+      img: '/assets/siteN1.png',
+    },
+    {
+      id: 4,
+      img: '/assets/siteN2.png',
+    },
+    {
+      id: 5,
+      img: '/assets/siteN3.png',
+    },
+    {
+      id: 6,
+      img: '/assets/siteN4.png',
+    },
+    {
+      id: 7,
+      img: '/assets/siteN5.png',
+    },
+    {
+      id: 8,
+      img: '/assets/siteN6.png',
+    },
+    {
+      id: 9,
+      img: '/assets/siteN7.png',
+    },
+    {
+      id: 10,
+      img: '/assets/siteN8.png',
+    },
+    {
+      id: 11,
+      img: '/assets/siteN9.png',
     },
   ];
 
